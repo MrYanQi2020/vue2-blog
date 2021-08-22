09e88b689f90e793bb8f3f1087e56aa0a3b016a7
mryanqi2020 <mryanqi2019@163.com>
2021-08-22 13:19:50 +0800

    添加头像组件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
+     <HeadPortrait src="https://i03piccdn.sogoucdn.com/89a49ed3594f7aef" alt="主页头像" size='150'/>
   </div>
 </template>
 
 <script>
+import HeadPortrait from "@/components/HeadPortrait";
 
 export default {
   name: 'App',
   components: {
+    HeadPortrait,
   }
 }
 </script>
---
+++ src/components/HeadPortrait/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="Head-portrait-container">
+      <img :src="src" :alt="alt" :style="{width:size+'px',height:size+'px'}">
+  </div>
+</template>
+
+<script>
+export default {
+    name:"HeadPortrait",
+    props:{
+        src:{
+            type:String,
+            require:false,
+            default:"https://img04.sogoucdn.com/app/a/100520093/8379901cc65ba509-45c21ceb904429fc-3d76954af80d02665d56409b836b4f63.jpg",
+        },
+        alt:{
+            type:String,
+            require:false,
+            default:"这是一张占位头像"
+        },
+        size:{
+            type:String,
+            require:false,
+            default:100
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .Head-portrait-container{
+        width:100%;
+        height:100%;
+        img{
+            display:inline-block;
+            border-radius:50%;
+
+        }
+    }
+
+</style>
