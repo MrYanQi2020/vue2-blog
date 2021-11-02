57c29d209d5bda2e490d621db807c59638d0daf6
mryanqi2020 <mryanqi2019@163.com>
2021-11-02 12:28:20 +0800

    leftAside样式调整

--- src/components/LeftAside/index.vue
+++ src/components/LeftAside/index.vue
@@ -49,5 +49,36 @@ export default {
         .Link{
             padding:40px 0;
         }
+        scrollbar-width: none; /* firefox */
+        -ms-overflow-style: none; /* IE 10+ */
+        overflow-x: hidden;
+        overflow-y: auto;
+        overflow-y: auto;
+        &::-webkit-scrollbar {
+            // display: none; /* Chrome Safari Edge */
+        }
+        /* 设置滚动条的样式 */
+        &::-webkit-scrollbar {
+            width: 0.4em;
+        }
+        /* 滚动槽 */
+        &::-webkit-scrollbar-track {
+            box-shadow: rgba(0, 0, 0, 0.3);
+            border-radius: 10px;
+        }
+        /* 滚动条滑块 */
+        &::-webkit-scrollbar-thumb {
+            border-radius: 10px;
+            height: 2.5em;
+            background: rgba(236, 236, 236, 0.3);
+            border-radius: 2.5px;
+            margin: 15px 0;
+            cursor: pointer;
+            transition: 0.1s;
+        }
+        // 页面失去焦点时滚动滑块的样式
+        // &::-webkit-scrollbar-thumb:window-inactive {
+        //     background: rgba(255, 0, 0, 0.4);
+        // }
     }
 </style>
